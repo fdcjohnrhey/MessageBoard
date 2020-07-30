69cef19b4ef6ccbf881f92f29a28861fbac69f55
jpags.me <41568125+jpags1024@users.noreply.github.com>
2020-07-30 13:40:02 +0800

    commit 6

--- app/Controller/MessagesController.php
+++ app/Controller/MessagesController.php
@@ -108,7 +108,7 @@ class MessagesController extends AppController {
 			array_push($list,$value['User']);
 		}
 			
-    		return json_encode($list);	
+    	return json_encode($list);	
 	}
 
 	public function AddNewMessage(){
